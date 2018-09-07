59a4c0a677987cfc43b421e3539d303a1bee7d4b
Jordan Elliott <37377660+YourTechie@users.noreply.github.com>
2018-09-07 09:49:32 -0500

    Nearly completed all exercises

--- Chapter2/src/exercises/Eggs.java
+++ Chapter2/src/exercises/Eggs.java
@@ -1,15 +1,29 @@
 package exercises;
-
+import java.util.Scanner; 
 public class Eggs {
 
 	public static void main(String[] args) 
 	{
-		double dozenEggs = 3.25; 
-		double singleEgg = .45; 
+		Scanner input = new Scanner(System.in);
 		
+		int dozenEggs; 
+		int singleEgg; 
+		double dozenEggCost = 3.25; 
+		double singleEggCost = .45;
+		int eggsTotal; 
+		double eggsCost; 
+		System.out.println("How many eggs are ordered? >> ");
+		eggsTotal = input.nextInt();
 		
+		dozenEggs = eggsTotal / 12;
+		singleEgg = eggsTotal % 12;
 		
+		eggsCost = dozenEggs * dozenEggCost + singleEgg * singleEggCost;
 		
+		System.out.println("You ordered " + eggsTotal + " Eggs. Thats " + dozenEggs + " dozen eggs at "
+				+ dozenEggCost + " per dozen and " + singleEgg + " loose eggs at " + singleEggCost + " cents each for a total of "
+						+ eggsCost + ".");
+						 
 		
 
 	}
