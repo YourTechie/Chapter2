34a067789c1cae9a78c50e52947e6099982f3db1
Jordan Elliott <37377660+YourTechie@users.noreply.github.com>
2018-09-13 16:52:01 -0500

    Attempted gameZone and finished chapter 3 debugging

---
+++ Chapter2/src/gameZone/MadLib.java
@@ -0,0 +1,22 @@
+package gameZone;
+import java.util.Scanner; 
+public class MadLib {
+
+	public static void main(String[] args) 
+	{
+		char noun1;
+		char noun2; 
+		char adjective1;
+		char ptv1;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Give a noun >> ");
+		noun1 = input.nextChar();
+		
+		
+		
+		
+	}
+
+}
